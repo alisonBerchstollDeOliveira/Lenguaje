89e12e1acec149c46d14327e6bd2aed5d8cf72f1
User <User>
2016-03-30 05:15:43 -0400

    Alison Berchstoll

--- Proyecto1/src/py/edu/facitec/proyecto1/entidad/Cliente.java
+++ Proyecto1/src/py/edu/facitec/proyecto1/entidad/Cliente.java
@@ -1,13 +1,18 @@
 package py.edu.facitec.proyecto1.entidad;
-
+/**
+ * Clase que representa al objeto cliente
+ * @author Alison Berchstoll de Oliveira
+ *
+ */
 public class Cliente extends Persona {
+	//Atributo
 	private boolean activo;
-
+	//Constuctor
 	public Cliente() {
 		super();
 		this.activo=true;
 	}
-
+	//Getter y Setter
 	public boolean isActivo() {
 		return activo;
 	}
@@ -15,5 +20,5 @@ public class Cliente extends Persona {
 	public void setActivo(boolean activo) {
 		this.activo = activo;
 	}
-	
+	 
 }
